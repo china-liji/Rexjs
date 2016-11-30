da758262e5fbe740ea03ddb042422b076b8a58ff
Michael_Lee <liji7@huawei.com>
2016-11-30 09:12:14 +0800

    函数表达式测试代码

--- test/function.js
+++ test/function.js
@@ -8,17 +8,38 @@ test.true("带多参数的函数声明", "function a(a, b, c){}");
 test.true("带单个默认参数的函数声明", "function a(a = 1){}");
 test.true("带多个默认参数的函数声明", "function a(a = 1, b = 2, c, d = 4){}");
 test.true("带单个省略参数的函数声明", "function a(...a){}");
-test.true("带所有情况的参数的函数声明", "function a(a, b = 2, c, d = 4, ...e){}")
+test.true("带所有情况的参数的函数声明", "function a(a, b = 2, c, d = 4, ...e){}");
+test.true("最简单的匿名函数表达式", "!function(){}");
+test.true("最简单带名称的函数表达式", "!function a(){}");
+test.true("带单个参数的函数表达式", "!function(a){}");
+test.true("带多参数的函数表达式", "!function a(a, b, c){}");
+test.true("带单个默认参数的函数表达式", "!function a(a = 1){}");
+test.true("带多个默认参数的函数表达式", "!function a(a = 1, b = 2, c, d = 4){}");
+test.true("带单个省略参数的函数表达式", "!function a(...a){}");
+test.true("带所有情况的参数的函数表达式", "!function a(a, b = 2, c, d = 4, ...e){}");
+test.true("函数表达式换行后接其他语句", "!function (){}\nvar b = 1");
 
 test.true(
-	"最简单的函数声明",
+	"函数声明后面接表达式",
 	"function a(){}(1,2,3)",
 	false,
 	function(parser){
-		return parser.statements.length === 4 ? "" : "没有识别出 2 个语句"
+		return parser.statements.length === 4 ? "" : "没有识别出 2 个语句";
 	},
 	function(parser){
 		return parser.statements[1].expression instanceof Rexjs.FunctionExpression ? "" : "没有正确解析出函数表达式";
+	}
+);
+
+test.true(
+	"直接执行的函数表达式",
+	"!function a(){}(1,2,3)",
+	false,
+	function(parser){
+		return parser.statements.length === 3 ? "" : "语句被分离";
+	},
+	function(parser){
+		return parser.statements[1].expression.operand.operand instanceof Rexjs.FunctionExpression ? "" : "没有正确解析出函数表达式";
 	}
 );
 
@@ -52,6 +73,14 @@ test.false(
 	"function a(){}, 1",
 	function(parser, err){
 		return err.context.tag instanceof Rexjs.CommaTag ? "" : "没有捕获逗号标签";
+	}
+);
+
+test.false(
+	"函数表达式后面直接接其他语句",
+	"!function a(){} var b = 1",
+	function(parser, err){
+		return err.context.tag instanceof Rexjs.VarTag ? "" : "没有识别出 var 关键字";
 	}
 );
 
